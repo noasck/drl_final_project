2e463638b680e2e9acb84dfa4c522f8f3050a5fd
noasck <denter425@gmail.com>
2021-01-31 16:57:00 +0200

    minor fixes

--- utils/trainer.py
+++ utils/trainer.py
@@ -1,17 +1,11 @@
 from sklearn.ensemble import GradientBoostingRegressor
 import pickle
 
-# class Estimator:
-#     @staticmethod
-#     def fit(train_x, train_y):
-#         return GradientBoostingRegressor(loss='huber', learning_rate=0.11, n_estimators=100).fit(train_x, train_y)
-
 class Estimator:
     @staticmethod
     def fit(train_x, train_y):
-        with open('GradientBoostingRegressor.pickle', 'wb')as f:
-            pickle.dump(GradientBoostingRegressor(loss='huber', learning_rate=0.11, n_estimators=100).fit(train_x, train_y), f)
         return GradientBoostingRegressor(loss='huber', learning_rate=0.11, n_estimators=100).fit(train_x, train_y)
+
 
 
     @staticmethod
